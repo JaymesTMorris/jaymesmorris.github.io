f70d13aea1d62a5404517c07b6c7c20e63edcdab
Jaymes T Morris <Jaymestmorris16@gmail.com>
2025-11-25 22:37:16 -0500

    Update experience.js to remove code private message

--- experience.js
+++ experience.js
@@ -2,30 +2,22 @@ const softwareProjects = [
   {
     title: "ASRC Federal Software Engineer Intern",
     type: "Jan 2025 - Present",
-    desc: "This is a description of my internship role at ASRC Federal.",
-    link: "#",
-    code: "#",
+    desc: "This is a description of my internship role at ASRC Federal."
   },
   {
     title: "Student Tech Lead at Rowan University",
     type: "Sep 2024 - Present",
-    desc: "This is a description of my role as a Student Tech Lead at Rowan University.",
-    link: "#",
-    code: "#",
+    desc: "This is a description of my role as a Student Tech Lead at Rowan University."
   },
   {
     title: "Crytptologic Technician Technical (CTT) - US Navy Reserves",
     type: "June 2024 - Present",
-    desc: "This is a description of my service as a Cryptologic Technician Technical in the US Navy Reserves.",
-    link: "#",
-    code: "#",
+    desc: "This is a description of my service as a Cryptologic Technician Technical in the US Navy Reserves."
   },
   {
     title: "Crytptologic Technician Technical (CTT) - US Navy [ACTIVE DUTY]",
     type: "Feb 2019 - Feb 2024",
-    desc: "This is a description of my service as a Cryptologic Technician Technical in the US Navy.",
-    link: "#",
-    code: "#",
+    desc: "This is a description of my service as a Cryptologic Technician Technical in the US Navy."
   }
 ];
 
@@ -39,20 +31,12 @@ function loadSoftware() {
     const card = document.createElement("div");
     card.classList.add("project-card");
 
-    const codeLink =
-      proj.code !== "#"
-        ? `<a href="${proj.code}" target="_blank">View Code</a>`
-        : `<span style="color:#777; font-size:0.9rem;">Code Private</span>`;
-
     card.innerHTML = `
       <h3>
-         <a href="${proj.link}" target="_blank">${proj.title}</a>
+         <a>${proj.title}</a>
       </h3>
       <p>${proj.desc}</p>
-      <span class="skills-badge" style="font-size:0.75rem; background:#444; padding: 4px 10px;">${proj.type.toUpperCase()}</span>
-      <br><br>
-      ${codeLink}
-    `;
+      <span class="skills-badge" style="font-size:0.75rem; background:#444; padding: 4px 10px;">${proj.type.toUpperCase()}</span>`;
     container.appendChild(card);
   });
 }
