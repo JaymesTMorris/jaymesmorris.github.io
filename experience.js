17693164ad288a6388519d096ae06972021ecd51
Jaymes T Morris <Jaymestmorris16@gmail.com>
2025-11-26 00:00:15 -0500

    Update experience descriptions

--- experience.js
+++ experience.js
@@ -2,22 +2,32 @@ const softwareProjects = [
   {
     title: "ASRC Federal Software Engineer Intern",
     type: "Jan 2025 - Present",
-    desc: "This is a description of my internship role at ASRC Federal."
+    desc: "I support ASRC Federal Mission Solutions as part of Rowan University’s research team, helping \
+    modernize U.S. Navy combat-system interfaces using C++ and Qt (a C++ UI framework) to improve usability \
+    and support current naval fleet requirements. I focus on improving usability, updating legacy components, \
+    and contributing to ongoing modernization work."
   },
   {
     title: "Student Tech Lead at Rowan University",
     type: "Sep 2024 - Present",
-    desc: "This is a description of my role as a Student Tech Lead at Rowan University."
+    desc: "Oversees technical equipment in the Rowan University TechLounge & MakerSpace, including 3D \
+    printers, 3D scanners, and robotic arms. Responsibilities include participating in the interviewing and \
+    hiring of 24 student workers and developing detailed user guides covering setup, operation, and closing \
+    procedures for lab equipment."
   },
   {
     title: "Crytptologic Technician Technical (CTT) - US Navy Reserves",
     type: "June 2024 - Present",
-    desc: "This is a description of my service as a Cryptologic Technician Technical in the US Navy Reserves."
+    desc: "Continuing service in the U.S. Navy Reserves while pursuing academic and professional goals, \
+    maintaining an active clearance."
   },
   {
-    title: "Crytptologic Technician Technical (CTT) - US Navy [ACTIVE DUTY]",
+    title: "Crytptologic Technician Technical (CTT) - US Navy ACTIVE DUTY",
     type: "Feb 2019 - Feb 2024",
-    desc: "This is a description of my service as a Cryptologic Technician Technical in the US Navy."
+    desc: "Served as a Cryptologic Technician Technical and Work Center Supervisor, leading a division of \
+    20 sailors and ensuring the completion of corrective and preventive maintenance across mission-critical \
+    systems. Responsibilities included training and mentoring junior personnel, maintaining operational \
+    readiness, and supporting anti-ship cruise missile defense and electronic warfare operations."
   }
 ];
 
